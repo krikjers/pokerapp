b5f57fdd5e5ff50de6a660b51a9336cc28c7fc5d
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-12-29 14:53:30 +0100

    Add funcions to get values from GUI

--- calculations.js
+++ calculations.js
@@ -1,1 +1,42 @@
-console.log('JS loaded')
+document.getElementById("calculate").onclick = calculate;
+document.getElementById("reset").onclick = resetForm;
+
+/**
+ * Calculate result
+ */
+function calculate() {
+    console.log('calculate')
+
+    const outs = getOuts().value
+    const pot = getPot().value
+    const bet = getBet().value
+    const round = getRound().value
+
+}
+
+function getRound() {
+    return document.querySelector('input[name = "round"]:checked');
+}
+
+function getOuts() {
+    return document.getElementById('outs')
+}
+
+function getPot() {
+    return document.getElementById('pot')
+}
+
+function getBet() {
+    return document.getElementById('bet')
+}
+
+function setResult(result) {
+    document.getElementById('result').innerHTML = result
+}
+
+/**
+ * Reset form
+ */
+function resetForm() {
+    console.log('reset')
+}
